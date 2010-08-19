86991cac4df1cae01f4417412e803e16852acb55
dimilar <dimilar@atradig107.ias.loc>
2010-08-19 19:11:40 +0200

    commentation for snake

--- ccd/src/ros_to_openCv.cpp
+++ ccd/src/ros_to_openCv.cpp
@@ -15,6 +15,9 @@ public:
     ImageConverter(ros::NodeHandle &n, char **argv) :
         n_(n), it_(n_), resolution_(1000), contour_(new CvPoint[resolution_])
         {
+            // manually draw contour using b-spline method
+            // but currently we generate the curve using canny edge dectection
+            // in future, we can use it
             int m ,t;
             m = 16;
             t = 3;
@@ -66,35 +69,33 @@ public:
                 {
                     ROS_ERROR("error");
                 }
-            int ialpha = 20;
-            int ibeta= 10;
-            int igamma=20;
-
             IplImage* img1 = cvCreateImage(cvGetSize(cv_image),IPL_DEPTH_8U,1);
+
+            // convert original image to gray-scale image
             cvCvtColor(cv_image,img1,CV_RGB2GRAY);
+
+            // set the ROI
             cvSetImageROI(img1, cvRect(0, 0, 640, 360 ));
             IplImage* img2 = cvCreateImage(cvGetSize(img1),IPL_DEPTH_8U,1);
+
+            // smooth-> canny edge dectection -> dilation -> erosion
             cvSmooth( img1, img1);
             cvCanny(img1, img2 ,0,80);
             cvDilate(img2, img2, NULL, 10);
             cvErode(img2, img2, NULL, 10);            
+
             CvMemStorage* storage = cvCreateMemStorage(0);
             CvSeq* contours = 0;
+            // find contour, save the contour into as a CvSeq structure
             cvFindContours( img2, storage, &contours, sizeof(CvContour), CV_RETR_EXTERNAL, 2);
+
+            // if contour's length is 0 or too small, skip it
             if(!contours) return ;
             int length = contours->total;
             std::cout<< "length: " << length << std::endl;
             if(length<10) return ; 
-            // for(CvSeq *c = contours ; c != NULL; c = contours->h_next)
-            //     {
-            //         // std::cout<< "hello" << std::endl;
-            //         /* replace CV_FILLED with 1 to see the outlines */
-            //         cvDrawContours( dst, c, color, color, -1, 2, 8 );
-            //     }
 
-            // // cvShowImage( "Image window", dst);
-            // cvReleaseImage(&dst);
-            // int length = contours->total;
+            // convert the contour into CvPoints
             CvPoint* point = new CvPoint[length];
             CvSeqReader reader;
             CvPoint pt= cvPoint(0,0);
@@ -106,27 +107,34 @@ public:
                     point[i] = pt;
                 }
             cvReleaseMemStorage(&storage);
+
+            // draw the contour on the origin image
             for(int i=0;i<length;i++){
             	int j = (i+1)%length;
-                // cout << point[i].x << " " << point[i].y << endl;
             	cvLine( cv_image, point[i], point[j], CV_RGB( 0, 0, 255 ), 1, 8, 0); 
             }
+
+            // set up parameters for snake algorithms
+            int ialpha = 20;
+            int ibeta= 10;
+            int igamma=20;
             float alpha=ialpha/100.0f; 
             float beta=ibeta/100.0f;
             float gamma=igamma/100.0f;
-            CvSize size; 
-            size.width=3; 
-            size.height=3; 
+            CvSize size ={ 3, 3}; 
             CvTermCriteria criteria; 
             criteria.type = CV_TERMCRIT_ITER; 
             criteria.max_iter = 1000; 
-            criteria.epsilon = 0.01; 
-            cvSnakeImage( img1, point, length, &alpha, &beta, &gamma,1,size,criteria,0 );
+            criteria.epsilon = 0.01;
+
+            // call cvSnakeImage
+            cvSnakeImage( img1, point, length, &alpha, &beta, &gamma,1,size,criteria,0 );            
             for(int i=0;i<length;i++)
                 {
                     int j = (i+1)%length;
                     cvLine( cv_image, point[i],point[j],CV_RGB( 0, 255, 0 ),1,8,0 );
                 }
+            
             cvShowImage("Image window", cv_image);
             n_.getParam("save_image", save_image_);
             cvReleaseImage(&img1);
@@ -148,9 +156,15 @@ protected:
     CvPoint *contour_;
     std::string save_image_;
 };
+
 int main(int argc, char** argv)
 {
-    if(argc < 1) exit(-1);
+    if(argc < 1)
+        {
+            exit(-1);
+            std::cout << "usage: ros_to_openCv topic" << std::endl;
+        }
+
     ros::init(argc, argv, "ros_to_openCv");
     ros::NodeHandle n("~");
     ImageConverter ic(n, argv);
