072027296631f994269bd36648166dc9a1daacaf
Dimilar Zhu <zhus@in.tum.de>
2011-04-05 18:09:38 +0200

    1. add sift.c 2.pass parameters from command line

--- ccd/src/ccd_node2.cpp
+++ ccd/src/ccd_node2.cpp
@@ -20,15 +20,16 @@ class CCDNode
   image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
   sensor_msgs::CvBridge bridge_;
-  std::vector<CvPoint2D64f> pts1;
   std::string image_topic_;
   CCD ccd;
   int count_;
+  int init_method_;
   CCDNode(ros::NodeHandle &n) :
   n_(n), it_(n_)
   {
     // n_.param("image_topic", image_topic_, std::string("/narrow_stereo/left/image_rect"));
     n_.param("image_topic", image_topic_, std::string("/wide_stereo/left/image_rect"));
+    n_.param("init_method", init_method_, 1);
     image_sub_ = it_.subscribe(image_topic_, 1, &CCDNode::imageCallback, this);
     ROS_INFO("CCDNode Ready, listening on topic %s", image_topic_.c_str());
     ccd.read_params(std::string("ccd_params.xml"));
@@ -63,22 +64,10 @@ class CCDNode
 
     std::cerr << "I got a new image" << std::endl;
 
-    //cv::GaussianBlur(cv_image, img, cv::Size(9,9), 0);
-    // cv::imshow("Origianl", img);
     if (count_ == 1)
     {
-      // cvSetMouseCallback( "Original", on_mouse,  (void*)&this->ccd);
-      ccd.init_pts(1);
-      // cvShowImage("Original",img1);
-      // for closed curves, we have to append 3 more points
-      // to the end, these 3 new points are the three one
-      // located in the head of the array
-      // if(ccd.pts.size() > params[9])
-      //   for (int i = 0; i < params[9]; ++i)
-      //     ccd.pts.push_back(ccd.pts[i]);
-        
-      // for (int i = 0; i < ccd.pts.size(); i++)
-      //   std::cerr << "pts initialized: " << ccd.pts[i].x << " " << ccd.pts[i].y << std::endl;
+      ccd.tpl = cv::imread("../data/book.png", 1);
+      ccd.init_pts(init_method_);
         
       BSpline bs(ccd.degree(), ccd.resolution(), ccd.pts);    
 
