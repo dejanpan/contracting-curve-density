b6cbcb1962c130a73c88a047f2389ee1cd0c0522
dimilar <dimilar@dimilar-debian.localdomain>
2011-05-02 21:17:32 +0200

    em algorithm test

--- ccd/src/em.cpp
+++ ccd/src/em.cpp
@@ -1,102 +1,98 @@
+#include "opencv/cv.h"
 #include "opencv/ml.h"
 #include "opencv/highgui.h"
 using namespace std;
-using namespace cv;
 int main( int argc, char** argv )
 {
-    const int N = 4;
-    const int N1 = (int)sqrt((double)N);
-    const CvScalar colors[] = {{0,0,255}},{{0,255,0}},{{0,255,255}},{{255,255,0}};
-    int i, j;
-    int nsamples = 100;
-    CvRNG rng_state = cvRNG(-1);
-    CvMat* samples = cvCreateMat( nsamples, 2, CV_32FC1 );
-    CvMat* labels = cvCreateMat( nsamples, 1, CV_32SC1 );
-    IplImage* img = cvCreateImage( cvSize( 500, 500 ), 8, 3 );
-    float _sample[2];
-    CvMat sample = cvMat( 1, 2, CV_32FC1, _sample );
-    CvEM em_model;
-    CvEMParams params;
-    CvMat samples_part;
-
-    cvReshape( samples, samples, 2, 0 );
-    for( i = 0; i < N; i++ )
+  const int MAX_CLUSTERS = 10;
+  cv::Vec3b colorTab[] =
+      {
+        cv::Vec3b(0, 0, 255),
+        cv::Vec3b(0,255,0),
+        cv::Vec3b(255,100,100),
+        cv::Vec3b(255,0,255),
+        cv::Vec3b(0,255,255),
+        cv::Vec3b(1,55,25),
+        cv::Vec3b(0,255,100),
+        cv::Vec3b(5,9,199),
+        cv::Vec3b(55,255,55),
+        cv::Vec3b(4,25,55)
+      };
+  
+  cv::Mat im = cv::imread(argv[1]);
+  cv::Mat_<cv::Vec3b>& imP = (cv::Mat_<cv::Vec3b>&)im;
+  
+  cv::Mat imageShow= cv::Mat::zeros(im.rows, im.cols, CV_8UC3);
+  int sampleCount = im.rows*im.cols;
+  CvMat *image = cvCreateMat(sampleCount, 3, CV_32FC1);
+  // im.convertTo(image, CV_32FC3, 1, 0);
+  float *imagePtr = image->data.fl;
+  int step = image->step/sizeof(float);
+  for (int i = 0; i < im.rows; ++i)
+  {
+    for (int j = 0; j < im.cols; ++j)
     {
-        CvScalar mean, sigma;
-
-        // form the training samples
-        cvGetRows( samples, &samples_part, i*nsamples/N,
-                                           (i+1)*nsamples/N );
-        mean = cvScalar(((i
-                       ((i/N1)+1.)*img->height/(N1+1));
-        sigma = cvScalar(30,30);
-        cvRandArr( &rng_state, &samples_part, CV_RAND_NORMAL,
-                                                        mean, sigma );
+      (imagePtr + (i*im.cols+j)*step)[0] = imP(i,j)[0];
+      (imagePtr + (i*im.cols+j)*step)[1] = imP(i,j)[1];
+      (imagePtr + (i*im.cols+j)*step)[2] = imP(i,j)[2];
     }
-    cvReshape( samples, samples, 1, 0 );
+  }
+  // imshow("em", image);
+  // cv::waitKey(-1);
+  int clusterCount = MIN(atoi(argv[2]), MAX_CLUSTERS);
+  
+  // cv::Mat labels = cv::Mat::zeros( sampleCount, 1, CV_32SC1);
+  CvMat* labels = cvCreateMat( sampleCount, 1, CV_32SC1 );
+  CvEM em_model;
+  CvEMParams params;
 
-    // initialize model's parameters
-    params.covs      = NULL;
-    params.means     = NULL;
-    params.weights   = NULL;
-    params.probs     = NULL;
-    params.nclusters = N;
-    params.cov_mat_type       = CvEM::COV_MAT_SPHERICAL;
-    params.start_step         = CvEM::START_AUTO_STEP;
-    params.term_crit.max_iter = 10;
-    params.term_crit.epsilon  = 0.1;
-    params.term_crit.type     = CV_TERMCRIT_ITER|CV_TERMCRIT_EPS;
 
-    // cluster the data
-    em_model.train( samples, 0, params, labels );
+  // initialize model's parameters
+  params.covs      = NULL;
+  params.means     = NULL;
+  params.weights   = NULL;
+  params.probs     = NULL;
+  params.nclusters = clusterCount;
+  params.cov_mat_type       = CvEM::COV_MAT_SPHERICAL;
+  params.start_step         = CvEM::START_AUTO_STEP;
+  params.term_crit.max_iter = 10;
+  params.term_crit.epsilon  = 0.1;
+  params.term_crit.type     = CV_TERMCRIT_ITER|CV_TERMCRIT_EPS;
+
+  // cluster the data
+  em_model.train( image, 0, params, labels);
 
 #if 0
-    // the piece of code shows how to repeatedly optimize the model
-    // with less-constrained parameters
-    //(COV_MAT_DIAGONAL instead of COV_MAT_SPHERICAL)
-    // when the output of the first stage is used as input for the second.
-    CvEM em_model2;
-    params.cov_mat_type = CvEM::COV_MAT_DIAGONAL;
-    params.start_step = CvEM::START_E_STEP;
-    params.means = em_model.get_means();
-    params.covs = (const CvMat**)em_model.get_covs();
-    params.weights = em_model.get_weights();
+  // the piece of code shows how to repeatedly optimize the model
+  // with less-constrained parameters
+  //(COV_MAT_DIAGONAL instead of COV_MAT_SPHERICAL)
+  // when the output of the first stage is used as input for the second.
+  CvEM em_model2;
+  params.cov_mat_type = CvEM::COV_MAT_DIAGONAL;
+  params.start_step = CvEM::START_E_STEP;
+  params.means = em_model.get_means();
+  params.covs = (const CvMat**)em_model.get_covs();
+  params.weights = em_model.get_weights();
 
-    em_model2.train( samples, 0, params, labels );
-    // to use em_model2, replace em_model.predict()
-    // with em_model2.predict() below
+  em_model2.train( image, 0, params, labels);
+  // to use em_model2, replace em_model.predict()
+  // with em_model2.predict() below
 #endif
-    // classify every image pixel
-    cvZero( img );
-    for( i = 0; i < img->height; i++ )
+  // classify every image pixel
+
+  for (int i = 0 ; i < im.rows; ++i)
+  {
+    for (int j = 0; j < im.cols; ++j)
     {
-        for( j = 0; j < img->width; j++ )
-        {
-            CvPoint pt = cvPoint(j, i);
-            sample.data.fl[0] = (float)j;
-            sample.data.fl[1] = (float)i;
-            int response = cvRound(em_model.predict( &sample, NULL ));
-            CvScalar c = colors[response];
-
-            cvCircle( img, pt, 1, cvScalar(c.val[0]*0.75,
-                c.val[1]*0.75,c.val[2]*0.75), CV_FILLED );
-        }
+      int imgRow = i*im.cols+j;
+      imageShow.at<cv::Vec3b>(i,j) = colorTab[labels->data.i[imgRow]];
     }
-
-    //draw the clustered samples
-    for( i = 0; i < nsamples; i++ )
-    {
-        CvPoint pt;
-        pt.x = cvRound(samples->data.fl[i*2]);
-        pt.y = cvRound(samples->data.fl[i*2+1]);
-        cvCircle( img, pt, 1, colors[labels->data.i[i]], CV_FILLED );
-    }
-
-    cvNamedWindow( "EM-clustering result", 1 );
-    cvShowImage( "EM-clustering result", img );
-    cvWaitKey(0);
-
-    cvReleaseMat( &samples );
-    cvReleaseMat( &labels );
-    return 0;
+  }
+  cv::namedWindow("cluster",1);
+  cv::imshow("clusters", imageShow);
+  cv::waitKey(0);
+  
+  cvReleaseMat(&image);
+  cvReleaseMat(&labels);
+  return 0;
 }
